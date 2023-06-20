13d8e340a4755d939da9bcd50aeab667c4aa7df2
Henrique <rickfernandez03@gmail.com>
2023-06-20 15:57:36 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'meteora-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'meteora-store';
+}
