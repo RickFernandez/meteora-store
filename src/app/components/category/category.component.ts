4679d176993cceebfc187df32d63eb78918d3a6c
Henrique <rickfernandez03@gmail.com>
2023-06-20 22:36:06 -0300

    feat: category section complete

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,9 +10,9 @@ export class CategoryComponent {
 
   categoryList: ICategory[] = [
     { name: 'Camisetas', imgSrc: '../../../assets/Desktop/Categorias/Categoria camiseta.png' },
-    { name: 'Bolsas', imgSrc: '../../../assets/Desktop/Categorias/Categoria Bolsas.png' },
+    { name: 'Bolsas', imgSrc: '../../../assets/Desktop/Categorias/Categoria Bolsa.png' },
     { name: 'Calçados', imgSrc: '../../../assets/Desktop/Categorias/Categoria Calçados.png' },
-    { name: 'calças', imgSrc: '../../../assets/Desktop/Categorias/Categoria calças.png' },
+    { name: 'calças', imgSrc: '../../../assets/Desktop/Categorias/Categoria calça.png' },
     { name: 'Casacos', imgSrc: '../../../assets/Desktop/Categorias/Categoria casacos.png' },
     { name: 'Óculos', imgSrc: '../../../assets/Desktop/Categorias/Categoria óculos.png' },
   ]
